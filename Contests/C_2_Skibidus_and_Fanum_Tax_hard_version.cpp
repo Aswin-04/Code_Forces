efd754c2efe27b0e452c0958e6757117d22a90de
Aswin-04 <e.aswin004@gmail.com>
2025-02-11 06:28:07 +0530

    Solved Problem C_2_Skibidus_and_Fanum_Tax_hard_version

---
+++ Contests/C_2_Skibidus_and_Fanum_Tax_hard_version.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define sp " "
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first 
+#define ss second 
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define ctz __builtin_ctz
+#define ctzll __builtin_ctzll
+#define clz __builtin_clz
+#define clzll __builtin_clzll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef vector<long long> vll;
+typedef map<int, int> mii;
+typedef set<int> si;
+typedef set<char> sc;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(ll x) {cerr << x;}
+void _print(int x) {cerr << x;}
+void _print(string x) {cerr << x;}
+void _print(char x) {cerr << x;}
+void _print(ld x) {cerr << x;}
+void _print(double x) {cerr << x;}
+void _print(ull x) {cerr << x;}
+
+template<class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}"; }
+template<class T> void _print(vector<T> v) {cerr << "["; for (T i: v) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(vector<vector<T>> v) {cerr << "[\n"; cerr << "  "; for(vector<T> i: v) {_print(i);} cerr << nline; cerr << "]";}
+template<class T> void _print(set<T> s) {cerr << "["; for (T i: s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(unordered_set<T> s) {cerr << "["; for (T i: s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T, class V> void _print(map<T, V> m) {cerr << "[\n"; cerr << "  "; for(auto i: m) {_print(i);} cerr << nline; cerr << "]";}
+template<class T, class V> void _print(unordered_map<T, V> m) {cerr << "[\n"; cerr << "  "; for(auto i: m) {_print(i);} cerr << nline; cerr << "]";}
+
+
+ll get_lower_bound(ll target, const vll &arr) {
+  int low = 0;
+  int high = sz(arr)-1;
+  ll ans = -1;
+
+  while(low <= high) {
+    int mid = low + ((high-low) >> 1);
+    if(arr[mid] >= target) {
+      ans = arr[mid];
+      high = mid-1;
+    }
+    else low = mid+1;
+  }
+  return ans;
+}
+
+// Overall TC: O(m log(m) + n log(m))
+
+void solve() {
+    ll n, m;
+    cin >> n;
+    cin >> m;
+
+    vll a(n);
+    vll b(m);
+
+    for(auto &i: a) cin >> i;
+    for(auto &i: b) cin >> i;
+
+    sort(all(b)); // O(m log m)
+
+    ll prev = -INF;
+
+    // O(n log m)
+    for(ll i=0; i < n; i++) {
+      ll val = get_lower_bound((prev+a[i]), b);
+      if(val != -1) {
+        if(prev > a[i]) a[i] = val-a[i];
+        else a[i] = min(a[i], val-a[i]);
+      }
+      if(a[i] < prev) {
+        cout << "NO" << nline;
+        return;
+      }
+      prev = a[i];
+    }
+
+    cout << "YES" << nline;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
