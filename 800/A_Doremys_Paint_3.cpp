1e01cd83c235289dfc8445e5cccb4905087e3254
Aswin-04 <e.aswin004@gmail.com>
2025-03-13 22:29:39 +0530

    Update problem A_Doremys_Paint_3

--- 800/A_Doremys_Paint_3.cpp
+++ 800/A_Doremys_Paint_3.cpp
@@ -2,37 +2,20 @@
 using namespace std;
 
 void solve() {
+
     int n;
     cin >> n;
 
-    vector<int> v(n);
-    for(auto &it: v) cin >> it;
-
-
-    if(n == 2) {
-        cout << "YES\n";
-        return;
+    map<int, int> mpp;
+    for(int i=1; i <= n; i++) {
+        int x;
+        cin >> x;
+        mpp[x]++;
     }
 
-    unordered_map<int, int> mp;
-
-    for(int num: v) {
-        mp[num]++;
-    }
-
-    int cnt_1 = 0;
-    int cnt_2 = 0;
-
-    for(auto it: mp) {
-        if(it.second == 1) cnt_1++;
-        else cnt_2+=it.second;
-    }
-
-    cnt_2-=(2*cnt_1);
-    if(cnt_2 < 0) cout << "NO\n";
-    else if(cnt_2%3 == 0 || cnt_1 == 0) cout << "YES\n";
-    else cout << "NO\n";
-
+    if(mpp.size() > 2) cout << "NO" << endl;
+    else if(abs(mpp.begin()->second - mpp.rbegin()->second) <= 1) cout << "YES" << endl;
+    else cout << "NO" << endl;
 }
 
 int main() {
