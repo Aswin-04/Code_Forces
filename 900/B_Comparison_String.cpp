ee78c3d22775a9d9ae00c6c285f0d9f554f695a2
Aswin-04 <e.aswin004@gmail.com>
2025-03-29 12:39:37 +0530

    Update Problem B_Comparison_String

--- 900/B_Comparison_String.cpp
+++ 900/B_Comparison_String.cpp
@@ -63,21 +63,15 @@ void solve() {
     string s;
     cin >> s;
 
-    int cnt = 1;
     int maxi = 1;
-
-    for(int i=0; i < n-1; i++) {
-      if(s[i] != s[i+1]) {
-        cnt = 1;
-        continue;
-      }
-
-      cnt++;
+    int cnt = 1;
+    for(int i=1; i < n; i++) {
+      if(s[i] == s[i-1]) cnt++;
+      else cnt = 1;
       maxi = max(maxi, cnt);
-    }
+    } 
+
     cout << maxi+1 << nline;
-
-
 }
 
 int main() {
