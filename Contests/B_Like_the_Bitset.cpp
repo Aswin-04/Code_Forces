76014654202d23612d92bb702adac871a1fe15f8
Aswin-04 <e.aswin004@gmail.com>
2025-08-28 21:57:47 +0530

    Solved Problem B_Like_the_Bitset

---
+++ Contests/B_Like_the_Bitset.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define sp " "
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first 
+#define ss second 
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define ctz __builtin_ctz
+#define ctzll __builtin_ctzll
+#define clz __builtin_clz
+#define clzll __builtin_clzll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef map<int, int> mii;
+typedef set<int> si;
+typedef set<char> sc;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(ll x) {cerr << x;}
+void _print(int x) {cerr << x;}
+void _print(string x) {cerr << x;}
+void _print(char x) {cerr << x;}
+void _print(ld x) {cerr << x;}
+void _print(double x) {cerr << x;}
+void _print(ull x) {cerr << x;}
+
+template<class T, class V> void _print(pair<T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}"; }
+template<class T> void _print(vector<T> v) {cerr << "["; for (T i: v) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(vector<vector<T>> v) {cerr << "[\n"; cerr << "  "; for(vector<T> i: v) {_print(i);} cerr << nline; cerr << "]";}
+template<class T> void _print(set<T> s) {cerr << "["; for (T i: s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(unordered_set<T> s) {cerr << "["; for (T i: s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T, class V> void _print(map<T, V> m) {cerr << "[\n"; cerr << "  "; for(auto i: m) {_print(i);} cerr << nline; cerr << "]";}
+template<class T, class V> void _print(unordered_map<T, V> m) {cerr << "[\n"; cerr << "  "; for(auto i: m) {_print(i);} cerr << nline; cerr << "]";}
+
+
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  string s;
+  cin >> s;
+
+  int zero_cnt = 0;
+  int one_cnt = 0;
+  int i=0; 
+  int j=0;
+  while(j < k) {
+    if(s[j] == '0') zero_cnt++;
+    else one_cnt++;
+    j++;
+  }
+
+  if(!zero_cnt) {
+    cout << "NO\n";
+    return;
+  }
+
+  while(j < n) {
+    if(s[i] == '0') zero_cnt--;
+    else one_cnt--;
+    if(s[j] == '0') zero_cnt++;
+    else one_cnt++;
+
+    if(!zero_cnt) {
+      cout << "NO\n";
+      return;
+    }
+    i++;
+    j++;
+  }
+
+  cout << "YES\n";
+  vi ans(n);
+  int val = n;
+  for(int i=0; i < n; i++) {
+    if(s[i] == '0') ans[i] = val--;
+  }
+  for(int i=0; i < n; i++) {
+    if(s[i] == '1') ans[i] = val--;
+  }
+
+  for(auto i: ans) cout << i << sp;
+  cout << nline;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
